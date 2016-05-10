05895d606066832c440e2d7b3db09eb9ecab807a
Chau Hoang Duy <duych@bit-vietnam.com>
2016-05-10 13:44:08 +0700

    add style

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,14 @@ Category.create(:id => 1, :text => 'Kinh te')
 Category.create(:id => 2, :text => 'Chinh tri')
 Category.create(:id => 3, :text => 'The thao')
 Category.create(:id => 4, :text => 'Giao duc')
+
+(1..100).each do |i|
+  Post.create!(
+    title: "Lipsum Post #{i}", 
+    text: %{
+      Nullam hendrerit iaculis sodales. Curabitur varius nibh arcu, id molestie nibh fermentum vitae. Cras quis semper dui. Cras porttitor urna sit amet risus vehicula tempor. Maecenas quis tempor ligula. Donec et nibh eu leo volutpat placerat. Fusce vulputate elit in nisi pretium, vel fermentum mi fermentum. Mauris scelerisque, lectus non luctus ultricies, urna eros tincidunt risus, at varius sapien diam id erat.
+      },
+    category_id: i % 4 + 1
+  
+  )
+end
